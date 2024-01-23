804ca7bec396019f57630eb9668f1649f7cd500d
mujeebr <58116087+mujeebr@users.noreply.github.com>
2024-01-23 15:36:30 +0530

    main.py

--- main.py
+++ main.py
@@ -25,26 +25,7 @@ st.title("Odinschool's Data Analysis App")
 st.subheader("Created by Mujeeb")
 file=st.file_uploader("Upload a CSV for data analysis")
 if file is not None:
-    df=pd.read_csv(file)
-    st.write(df.head(5))
-
-
-    query=st.text_area("What do you want to know from the csv file?")  
-
-    
-    llm = OpenAI(api_token=os.getenv("API_KEY"))
-    df=SmartDataframe(df,config={"llm":llm})
-    if st.button("Fetch"):
-        if query:
-            with st.spinner("Fetching info"):
-                st.write(df.chat(query))
-
-
-        else:
-            st.warning("please enter your query")
-else:
-    
-    df=pd.read_excel(file)
+    df=pd.read_csv(file) or df=pd.read_excel(file)
     st.write(df.head(5))
 
 
